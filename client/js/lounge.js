22c155270e5228d30fe21999fbf0056bff12b075
Nathan Grebowiec <njgreb@gmail.com>
2016-10-19 15:42:11 +0000

    bring back the dickbutt

--- client/js/lounge.js
+++ client/js/lounge.js
@@ -10,6 +10,8 @@ $(function() {
 		"/connect",
 		"/deop",
 		"/devoice",
+		"/dickbutt",
+		"/dickbuttstats",
 		"/disconnect",
 		"/invite",
 		"/join",
@@ -702,6 +704,47 @@ $(function() {
 
 		if (text.indexOf("/clear") === 0) {
 			clear();
+			return;
+		}
+
+		if (text.indexOf("/dickbuttstats") === 0) {
+			$.ajax({
+				url: 'https://api-ssl.bitly.com/v3/user/clicks?access_token=baaead629b85afc6ebdbce575fdaf50eed602b4e',
+				success: function(response) {
+					var dickButtClicks = 0;
+
+					for(var i=0; i<response.data.clicks.length; i++) {
+						dickButtClicks += response.data.clicks[i].clicks;
+					}
+					text = dickButtClicks + ' dickbutt clicks in the past ' + response.data.days + ' days.';
+
+					socket.emit("input", {
+						target: chat.data("id"),
+						text: text
+					});
+				}, error: function(data) {
+					console.error(data);
+				}
+			});
+
+			return;
+		}
+
+		if (text.indexOf("/dickbutt") === 0) {
+			$.ajax({
+				url: 'https://api-ssl.bitly.com/v3/shorten?access_token=baaead629b85afc6ebdbce575fdaf50eed602b4e&longUrl=http%3A%2F%2Fi.kinja-img.com/gawker-media/image/upload/m5g6imznbymcxkbpwpfc.jpg%23' + Math.random().toString(36).substring(5),
+				success: function(response) {
+					text = response.data.url;
+
+					socket.emit("input", {
+						target: chat.data("id"),
+						text: text
+					});
+				}, error: function(data) {
+					console.error(data);
+				}
+			});
+
 			return;
 		}
 
